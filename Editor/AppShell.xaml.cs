bbd3e77892eb0963099dac8abf1fad71b5a7f943
xxorza <xxorza@proton.me>
2023-05-19 17:51:48 +0300

    added maui app

--- Editor/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyApp;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ csso.Nodeshop.Editor/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace MyApp;
+
+public partial class AppShell : Shell {
+    public AppShell() {
+        InitializeComponent();
+    }
+}
