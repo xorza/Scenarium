918ce21c6253a9307793ecf3a7d75e58e9f0590b
xxorza <xxorza@proton.me>
2023-05-19 19:24:59 +0300

    lol rider debugger fixed

--- csso.Nodeshop.Editor/Platforms/MacCatalyst/Program.cs
+++ csso.Nodeshop.Editor/Platforms/MacCatalyst/Program.cs
@@ -3,10 +3,12 @@
 namespace csso.Nodeshop.Editor;
 
 public class Program {
-    // This is the main entry point of the application.
     static void Main(string[] args) {
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
+#if DEBUG
+        //Rider debugger doesn't work without this
+        Thread.Sleep(2000);
+#endif
+
         UIApplication.Main(args, null, typeof(AppDelegate));
     }
 }
