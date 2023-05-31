b878a17a31a660c8c8b673dd55e5a86d96d98181
xxorza <xxorza@proton.me>
2023-05-31 18:41:54 +0300

    opencl as feature

--- Graph/src/lib.rs
+++ Graph/src/lib.rs
@@ -3,7 +3,11 @@
 mod runtime_tests;
 mod graph_tests;
 mod lua_invoker_tests;
-// mod ocl_tests;
+
+#[cfg(feature = "opencl")]
+mod ocl_tests;
+#[cfg(feature = "opencl")]
+pub mod ocl_context;
 
 pub mod common;
 pub mod runtime;
@@ -12,4 +16,3 @@ pub mod function_graph;
 pub mod invoke;
 pub mod lua_invoker;
 pub mod data_type;
-// pub mod ocl_context;
