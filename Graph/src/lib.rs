518ca5641458498e686532c0a0263b1154d8383f
xxorza <xxorza@proton.me>
2023-06-01 15:37:11 +0300

    moved tests to separate folder

--- Graph/src/lib.rs
+++ Graph/src/lib.rs
@@ -1,13 +1,7 @@
 #![allow(dead_code)]
 
 #[cfg(test)]
-mod runtime_tests;
-#[cfg(test)]
-mod graph_tests;
-#[cfg(test)]
-mod lua_invoker_tests;
-#[cfg(all(feature = "opencl", test))]
-mod ocl_tests;
+mod tests;
 
 #[cfg(feature = "opencl")]
 pub mod ocl_context;
---
+++ Graph/src/tests/mod.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod runtime_tests;
+#[cfg(test)]
+mod graph_tests;
+#[cfg(test)]
+mod lua_invoker_tests;
+#[cfg(all(feature = "opencl", test))]
+mod ocl_tests;
