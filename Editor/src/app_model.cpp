cc78e015ee7c1a6599c8885d10fcd497110d1eba
xxorza <1442523+xorza@users.noreply.github.com>
2023-06-17 17:05:38 +0300

    c_api in dev
    Signed-off-by: xxorza <1442523+xorza@users.noreply.github.com>

---
+++ Editor/src/app_model.cpp
@@ -0,0 +1,16 @@
+#include "app_model.hpp"
+
+#include "graph_c_api.hpp"
+
+AppModel::AppModel() {
+    graph_c_api::init();
+
+    auto functions = graph_c_api::get_functions();
+    for (auto &func: functions) {
+        m_functions.append(new QmlFunctionInfo(func, this));
+    }
+}
+
+AppModel::~AppModel() {
+    graph_c_api::deinit();
+}
---
+++ Editor/src/function_info.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QUuid>
+#include <QVariant>
+#include <utility>
+
+namespace graph_c_api {
+    struct FunctionInfo;
+}
+
+
+class QmlArgInfo : public QObject {
+Q_OBJECT
+
+    Q_PROPERTY(QString name READ name CONSTANT)
+    Q_PROPERTY(int type READ type CONSTANT)
+
+public:
+    explicit QmlArgInfo(
+            QString name,
+            int type,
+            QObject *parent
+    ) : QObject(parent), m_name(std::move(name)), m_type(type) {}
+
+    QString name() const { return m_name; }
+
+    int type() const { return m_type; }
+
+private:
+    QString m_name;
+    int m_type;
+};
+
+class QmlFunctionInfo : public QObject {
+Q_OBJECT
+
+    Q_PROPERTY(QString name READ name CONSTANT)
+    Q_PROPERTY(QUuid uuid READ uuid CONSTANT)
+    Q_PROPERTY(QList<QmlArgInfo *> inputs READ inputs CONSTANT)
+    Q_PROPERTY(QList<QmlArgInfo *> outputs READ outputs CONSTANT)
+
+public:
+    explicit QmlFunctionInfo(
+            QString name,
+            const QUuid &uuid,
+            QObject *parent
+    ) : QObject(parent), m_name(std::move(name)), m_uuid(uuid) {}
+
+    explicit QmlFunctionInfo(
+            const graph_c_api::FunctionInfo &func,
+            QObject *parent
+    );
+
+    const QString &name() const { return m_name; }
+
+    const QUuid &uuid() const { return m_uuid; }
+
+    [[nodiscard]] const QList<QmlArgInfo *> &inputs() const { return m_inputs; }
+
+    [[nodiscard]] const QList<QmlArgInfo *> &outputs() const { return m_outputs; }
+
+
+private:
+    QString m_name;
+    QUuid m_uuid;
+    QList<QmlArgInfo *> m_inputs;
+    QList<QmlArgInfo *> m_outputs;
+};
