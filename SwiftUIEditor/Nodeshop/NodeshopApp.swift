7d146603e339cbe2f442e8efb0f4965e5c1baa60
xxorza <1442523+xorza@users.noreply.github.com>
2023-06-18 10:04:05 +0300

    added swift ui proj
    Signed-off-by: xxorza <1442523+xorza@users.noreply.github.com>

---
+++ SwiftUIEditor/Nodeshop/NodeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeshopApp.swift
+//  Nodeshop
+//
+//  Created by illia tulupov on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct NodeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
