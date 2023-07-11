8af88f11d891f232b025c987d10151f85480a170
xxorza <1442523+xorza@users.noreply.github.com>
2023-07-11 19:55:10 +0300

    runtime graph
    Signed-off-by: xxorza <1442523+xorza@users.noreply.github.com>

--- Graph/src/tests/preprocess_tests.rs
+++ Graph/src/tests/preprocess_tests.rs
@@ -23,9 +23,34 @@ fn simple_run() -> anyhow::Result<()> {
     let graph = Graph::from_yaml_file("../test_resources/test_graph.yml")?;
     let runtime = Preprocess::default();
 
-    let nodes = runtime.run(&graph, &mut RuntimeGraph::default());
+    let runtime_graph = runtime.run(&graph, &mut RuntimeGraph::default());
+    assert_eq!(runtime_graph.nodes.len(), 5);
+    assert_eq!(runtime_graph.node_by_name("val2").unwrap().total_binding_count, 2);
+    assert!(runtime_graph.nodes.iter().all(|r_node| r_node.should_execute));
+    assert!(runtime_graph.nodes.iter().all(|r_node| !r_node.has_missing_inputs));
 
-    let _yaml = serde_yaml::to_string(&nodes)?;
+    let _yaml = serde_yaml::to_string(&runtime_graph)?;
+
+    Ok(())
+}
+
+#[test]
+fn missing_input() -> anyhow::Result<()> {
+    let mut graph = Graph::from_yaml_file("../test_resources/test_graph.yml")?;
+    graph.node_by_name_mut("sum").unwrap()
+        .inputs[0].binding = Binding::None;
+
+    let runtime = Preprocess::default();
+    let runtime_graph = runtime.run(&graph, &mut RuntimeGraph::default());
+    assert_eq!(runtime_graph.nodes.len(), 4);
+    assert_eq!(runtime_graph.node_by_name("val2").unwrap().total_binding_count, 2);
+    assert!(runtime_graph.nodes.iter().all(|r_node| r_node.should_execute));
+    assert!(!runtime_graph.node_by_name("val2").unwrap().has_missing_inputs);
+    assert!(runtime_graph.node_by_name("sum").unwrap().has_missing_inputs);
+    assert!(runtime_graph.node_by_name("mult").unwrap().has_missing_inputs);
+    assert!(runtime_graph.node_by_name("print").unwrap().has_missing_inputs);
+
+    let _yaml = serde_yaml::to_string(&runtime_graph)?;
 
     Ok(())
 }
