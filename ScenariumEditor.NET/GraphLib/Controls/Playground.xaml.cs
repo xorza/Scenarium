1f0d315817989344bf48d30bcd0670b03539ff21
xxorza <xxorza@gmail.com>
2024-05-20 16:48:47 +0300

    Added Playground UserControl and updated styles
    Added a new UserControl named "Playground" with associated styles and incorporated it into the MainWindow. Also enhanced the visual clarity by adjusting colors, borders, and adding more consistent padding and margin across various components. Added tabs to the playground control to provide a cleaner design and better organization of data.

---
+++ ScenariumEditor.NET/GraphLib/Controls/Playground.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace GraphLib.Controls;
+
+public partial class Playground : UserControl {
+    public Playground() {
+        InitializeComponent();
+    }
+}
