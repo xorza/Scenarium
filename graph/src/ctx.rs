4be0f56017dca93b347f8bd48d9b9abaf5743460
xxorza <1442523+xorza@users.noreply.github.com>
2024-05-22 10:25:36 +0300

    Update Context structure in ctx.rs
    The Context structure within ctx.rs has been revised. Notably, 'FuncLib' has
    
    Signed-off-by: xxorza <1442523+xorza@users.noreply.github.com>

--- graph/src/ctx.rs
+++ graph/src/ctx.rs
@@ -1,4 +1,5 @@
 use crate::compute::Compute;
+use crate::function::FuncLib;
 use crate::graph::Graph;
 use crate::invoke::UberInvoker;
 use crate::runtime_graph::RuntimeGraph;
@@ -8,8 +9,9 @@ use crate::worker::Worker;
 pub struct Context {
     pub(crate) graph: Graph,
     pub(crate) runtime_graph: RuntimeGraph,
-    pub(crate) invoker: UberInvoker,
     pub(crate) compute: Compute,
+    pub(crate) func_lib: FuncLib,
 
+    pub(crate) invoker: Option<UberInvoker>,
     pub(crate) worker: Option<Worker>,
 }
