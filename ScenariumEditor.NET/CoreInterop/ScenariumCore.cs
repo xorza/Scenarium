af70f810ce4e62a031c0b73ab4223b3df8b2815c
xxorza <1442523+xorza@users.noreply.github.com>
2025-02-21 18:46:51 +0200

    Refactor ScenariumCore by removing DLL loading/unloading logic and associated reference counting

--- ScenariumEditor.NET/CoreInterop/ScenariumCore.cs
+++ ScenariumEditor.NET/CoreInterop/ScenariumCore.cs
@@ -7,17 +7,6 @@ using YamlDotNet.Serialization.NamingConventions;
 namespace CoreInterop;
 
 public unsafe partial class ScenariumCore : IDisposable {
-    private static readonly Lock LOCK = new();
-    private static int _ref_count = 0;
-
-    [LibraryImport("kernel32.dll", EntryPoint = "LoadLibraryW", SetLastError = true,
-        StringMarshalling = StringMarshalling.Utf16)]
-    private static partial IntPtr LoadLibrary(string libname);
-
-    [LibraryImport("kernel32.dll", EntryPoint = "FreeLibrary")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static partial bool FreeLibrary(IntPtr h_module);
-
     internal const string DLL_NAME = "core_interop.dll";
 
     [LibraryImport(DLL_NAME)]
@@ -28,55 +17,18 @@ public unsafe partial class ScenariumCore : IDisposable {
     [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
     private static partial void destroy_context(void* ctx);
 
+    
     [LibraryImport(DLL_NAME)]
     [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
     private static partial FfiBuf get_graph(void* ctx);
-
-
+    
     [LibraryImport(DLL_NAME)]
     [UnmanagedCallConv(CallConvs = [typeof(System.Runtime.CompilerServices.CallConvCdecl)])]
     private static partial FfiBuf get_func_lib(void* ctx);
-
-
-    private static IntPtr _core_interop_handle = IntPtr.Zero;
-
-    private static void LoadDll() {
-        lock (LOCK) {
-            if (_core_interop_handle != IntPtr.Zero) return;
-
-            var sw = Stopwatch.StartNew();
-
-            var full_dll_path = Path.GetFullPath(DLL_NAME);
-
-            _core_interop_handle = LoadLibrary(full_dll_path);
-            if (_core_interop_handle == IntPtr.Zero) {
-                int error_code = Marshal.GetLastWin32Error();
-                throw new Exception($"Failed to load library (ErrorCode: {error_code})");
-            }
-
-            _ref_count++;
-
-            var elapsed = sw.ElapsedMilliseconds;
-        }
-    }
-
-    private static void UnloadDll() {
-        lock (LOCK) {
-            if (_core_interop_handle == IntPtr.Zero) return;
-            if (_ref_count == 0)
-                throw new Exception("Unloading library with 0 references");
-
-            FreeLibrary(_core_interop_handle);
-            _core_interop_handle = IntPtr.Zero;
-            _ref_count--;
-        }
-    }
-
-
+    
     private void* _ctx = null;
 
     public ScenariumCore() {
-        LoadDll();
         _ctx = create_context();
     }
 
@@ -116,7 +68,6 @@ public unsafe partial class ScenariumCore : IDisposable {
         if (_ctx != null)
             destroy_context(_ctx);
         _ctx = null;
-        UnloadDll();
     }
 
     public void Dispose() {
