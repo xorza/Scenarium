cd00e8660f9a41b7e25ea4ad970165448c608c93
xxorza <xxorza@gmail.com>
2025-11-11 18:42:28 +0200

    removed svelte+tauri gui app

--- ScenariumEditor.Svelte+Tauri/frontend/src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-export { default as Node } from "./Node.svelte";
-export { default as Graph } from "./Graph.svelte";
-export { default as FuncLibrary } from "./FuncLibrary.svelte";
-export { default as NodeDetails } from "./NodeDetails.svelte";
--- ScenariumEditor.Svelte+Tauri/src-tauri/src/main.rs
+++
@@ -1,7 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![allow(dead_code)]
-
-fn main() {
-    tauri_app_lib::run()
-}
