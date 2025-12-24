d5d93b6f52021b0edaf9f830ae23f2ee6ffea32b
xxorza <xxorza@gmail.com>
2025-12-24 18:22:15 +0200

    Refactor LuaCtx to use Arc<Mutex<mlua::Lua>> directly
    Remove unsafe raw pointer management and simplify async Lua calls.

--- graph/src/elements/lua/playground.rs
+++ graph/src/elements/lua/playground.rs
@@ -3,79 +3,31 @@ use std::sync::Arc;
 
 use tokio::sync::Mutex;
 
-trait SendSync: Sync + Send {}
-
-#[derive(Debug, Clone)]
-struct LuaCtxInternal {
-    lua: *mut mlua::Lua,
-}
-
-// It is safe to send and share `LuaCtxInternal` across threads because access
-// to the underlying Lua instance is synchronized through a `Mutex`.
-unsafe impl Send for LuaCtxInternal {}
-unsafe impl Sync for LuaCtxInternal {}
-
 #[derive(Debug, Clone)]
 struct LuaCtx {
-    inner: Arc<Mutex<LuaCtxInternal>>,
+    inner: Arc<Mutex<mlua::Lua>>,
 }
 
 impl Default for LuaCtx {
     fn default() -> Self {
         LuaCtx {
-            inner: Arc::new(Mutex::new(LuaCtxInternal::default())),
+            inner: Arc::new(Mutex::new(mlua::Lua::new())),
         }
     }
 }
 
 impl LuaCtx {
     async fn load(&self, script: &str) -> Result<(), mlua::Error> {
-        let guard = self.inner.lock().await;
-        guard.load(script)
+        let lua = self.inner.lock().await;
+        lua.load(script).exec()
     }
 
     async fn call<R: FromLuaMulti>(&self, func: &str) -> Result<R, mlua::Error> {
-        let guard = self.inner.lock().await;
-        guard.call(func)
-    }
-}
-
-impl Default for LuaCtxInternal {
-    fn default() -> Self {
-        let lua = Box::new(mlua::Lua::new());
-        let lua: *mut mlua::Lua = Box::into_raw(lua);
-        LuaCtxInternal { lua }
-    }
-}
-
-impl Drop for LuaCtxInternal {
-    fn drop(&mut self) {
-        unsafe {
-            drop(Box::from_raw(self.lua));
-        }
-    }
-}
-
-impl LuaCtxInternal {
-    fn load(&self, script: &str) -> Result<(), mlua::Error> {
-        let lua = unsafe { &mut *self.lua };
-        lua.load(script).exec()?;
-
-        Ok(())
-    }
-
-    fn call<R: FromLuaMulti>(&self, func: &str) -> Result<R, mlua::Error> {
-        let lua = unsafe { &mut *self.lua };
+        let lua = self.inner.lock().await;
         let func: mlua::Function = lua.globals().get(func)?;
-        let r: R = func.call(())?;
-
-        Ok(r)
+        func.call(())
     }
 }
-
-unsafe impl Send for LuaCtx {}
-
-unsafe impl Sync for LuaCtx {}
 
 #[cfg(test)]
 mod tests {
