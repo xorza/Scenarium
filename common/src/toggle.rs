ce7e806529aa3066ca536ddb0175704a10a89b22
xxorza <xxorza@gmail.com>
2025-12-24 21:44:13 +0200

    Remove unused `on` method from `Toggle` trait and implementation

--- common/src/toggle.rs
+++ common/src/toggle.rs
@@ -1,18 +1,11 @@
 pub trait Toggle {
     fn toggle(&mut self) -> bool;
-    fn on(&mut self) -> bool;
 }
 
 impl Toggle for bool {
     fn toggle(&mut self) -> bool {
         let was = *self;
         *self = !was;
-        was
-    }
-
-    fn on(&mut self) -> bool {
-        let was = *self;
-        *self = true;
         was
     }
 }
@@ -28,14 +21,5 @@ mod tests {
         assert!(toggle);
         assert!(toggle.toggle());
         assert!(!toggle);
-    }
-
-    #[test]
-    fn test_on() {
-        let mut toggle = false;
-        assert!(!toggle.on());
-        assert!(toggle);
-        assert!(toggle.on());
-        assert!(toggle);
     }
 }
