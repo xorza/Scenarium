05c467099894e47358dccb67e4313078e7c9edf9
xxorza <xxorza@gmail.com>
2025-12-24 21:36:06 +0200

    Simplify reformat_yaml by inlining variables

--- common/src/yaml_format.rs
+++ common/src/yaml_format.rs
@@ -2,7 +2,5 @@ use crate::normalize_string::NormalizeString;
 
 pub fn reformat_yaml(yaml: &str) -> anyhow::Result<String> {
     let value = serde_yml::from_str::<serde_yml::Value>(yaml)?;
-    let yaml = serde_yml::to_string(&value)?;
-    let yaml = yaml.normalize();
-    Ok(yaml)
+    Ok(serde_yml::to_string(&value)?.normalize())
 }
