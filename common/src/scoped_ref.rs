9f2c045136e1f579a8df4cb01c4867878895a325
xxorza <xxorza@gmail.com>
2025-12-29 13:34:30 +0200

    Simplify ScopeRef by removing data and parameterize closure only by FnOnce()
    Add tests to verify that the drop callback runs exactly once on scope
    exit

--- common/src/scoped_ref.rs
+++ common/src/scoped_ref.rs
@@ -1,94 +1,70 @@
-use std::ops::{Deref, DerefMut};
-
 #[derive(Debug)]
-pub struct ScopeRef<'a, T, F>
+pub struct ScopeRef<F>
 where
-    F: FnOnce(&T) + 'a,
+    F: FnOnce() + 'static,
 {
-    data: &'a T,
     on_drop: Option<F>,
 }
-#[derive(Debug)]
-pub struct ScopeRefMut<'a, T, F>
+impl<F> ScopeRef<F>
 where
-    F: FnOnce(&mut T) + 'a,
+    F: FnOnce() + 'static,
 {
-    data: &'a mut T,
-    on_drop: Option<F>,
-}
-
-impl<'a, T, F> ScopeRef<'a, T, F>
-where
-    F: FnOnce(&T) + 'a,
-{
-    pub fn new(data: &'a T, on_drop: F) -> Self {
+    pub fn new(on_drop: F) -> Self {
         Self {
-            data,
             on_drop: Some(on_drop),
         }
     }
 }
-impl<'a, T, F> Deref for ScopeRef<'a, T, F>
+impl<F> Drop for ScopeRef<F>
 where
-    F: FnOnce(&T) + 'a,
-{
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.data
-    }
-}
-impl<'a, T, F> Drop for ScopeRef<'a, T, F>
-where
-    F: FnOnce(&T) + 'a,
+    F: FnOnce() + 'static,
 {
     fn drop(&mut self) {
-        let on_drop = self
-            .on_drop
-            .take()
-            .expect("ScopeRef missing on_drop callback");
-        (on_drop)(self.data);
+        let on_drop = self.on_drop.take().expect("ScopeRef missing on_drop");
+        on_drop();
     }
 }
 
-impl<'a, T, F> ScopeRefMut<'a, T, F>
-where
-    F: FnOnce(&mut T) + 'a,
-{
-    pub fn new(data: &'a mut T, on_drop: F) -> Self {
-        Self {
-            data,
-            on_drop: Some(on_drop),
+#[cfg(test)]
+mod tests {
+    use super::ScopeRef;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::Arc;
+
+    #[test]
+    fn drop_runs_once() {
+        let counter = Arc::new(AtomicUsize::new(0));
+        {
+            let counter = Arc::clone(&counter);
+            let _guard = ScopeRef::new(move || {
+                counter.fetch_add(1, Ordering::SeqCst);
+            });
         }
-    }
-}
-impl<'a, T, F> Deref for ScopeRefMut<'a, T, F>
-where
-    F: FnOnce(&mut T) + 'a,
-{
-    type Target = T;
 
-    fn deref(&self) -> &Self::Target {
-        self.data
+        assert_eq!(
+            counter.load(Ordering::SeqCst),
+            1,
+            "ScopeRef should invoke its closure exactly once"
+        );
     }
-}
-impl<'a, T, F> DerefMut for ScopeRefMut<'a, T, F>
-where
-    F: FnOnce(&mut T) + 'a,
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.data
-    }
-}
-impl<'a, T, F> Drop for ScopeRefMut<'a, T, F>
-where
-    F: FnOnce(&mut T) + 'a,
-{
-    fn drop(&mut self) {
-        let on_drop = self
-            .on_drop
-            .take()
-            .expect("ScopeRefMut missing on_drop callback");
-        (on_drop)(self.data);
+
+    #[test]
+    fn drop_runs_on_scope_exit() {
+        let counter = Arc::new(AtomicUsize::new(0));
+
+        let value = {
+            let counter = Arc::clone(&counter);
+            let _guard = ScopeRef::new(move || {
+                counter.fetch_add(1, Ordering::SeqCst);
+            });
+            42
+        };
+
+        assert_eq!(value, 42);
+        assert_eq!(
+            counter.load(Ordering::SeqCst),
+            1,
+            "ScopeRef should invoke its closure on scope exit"
+        );
     }
 }
