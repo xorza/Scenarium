89c747cc4be2dd9c74da9ff19f3da7fbfd0fcde7
xxorza <xxorza@gmail.com>
2026-01-07 18:48:06 +0200

    Simplify node layout update by removing unused flag

--- editor/src/gui/graph_layout.rs
+++ editor/src/gui/graph_layout.rs
@@ -40,18 +40,15 @@ impl GraphLayout {
         let rect = ctx.ui.available_rect_before_wrap();
         self.origin = rect.min + view_graph.pan;
         let mut write_idx = 0;
-        let mut new_layout = false;
+
         for view_node in view_graph.view_nodes.iter() {
             let idx = self
                 .node_layouts
                 .compact_insert_with(&view_node.id, &mut write_idx, || {
-                    new_layout = true;
-                    node_layout::NodeLayout::new(ctx, view_graph, &view_node.id)
+                    NodeLayout::new(ctx, view_graph, &view_node.id)
                 });
 
-            if !new_layout {
-                self.node_layouts.items[idx].update(ctx, view_graph, &view_node.id, self.origin);
-            }
+            self.node_layouts.items[idx].update(ctx, view_graph, &view_node.id, self.origin);
         }
         self.node_layouts.compact_finish(write_idx);
     }
