e719c9070d48718cfc59aee6089f1a6897241136
xxorza <xxorza@gmail.com>
2026-01-11 17:42:40 +0200

    Adjust const badge vertical offset in GUI style

--- editor/src/gui/style.rs
+++ editor/src/gui/style.rs
@@ -229,7 +229,7 @@ impl Style {
                 port_radius: scaled(18.0 * 0.3),
                 port_activation_radius: scaled(18.0 * 0.3 * 1.3),
                 port_label_side_padding: scaled(8.0),
-                const_badge_offset: Vec2::new(scaled(-15.0), scaled(-25.0)),
+                const_badge_offset: Vec2::new(scaled(-15.0), scaled(-15.0)),
 
                 input_port_color: COLOR_PORT_INPUT,
                 output_port_color: COLOR_PORT_OUTPUT,
