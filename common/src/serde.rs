4eb41d26f1dcfe9ea8cd26f5a013b5db6d0b50a6
xxorza <xxorza@gmail.com>
2026-01-15 22:24:06 +0200

    Add LZ4 compression to SCN format serialization and deserialization

--- common/src/serde.rs
+++ common/src/serde.rs
@@ -41,31 +41,26 @@ pub fn serialize_into<T: Serialize, W: Write>(
         SerdeFormat::Bincode => {
             bincode::serde::encode_into_std_write(value, writer, bincode::config::standard())
                 .unwrap();
-
-            // // Prepend uncompressed size (4 bytes, little-endian)
-            // let uncompressed_size = temp_buffer.len();
-            // writer
-            //     .write_all(&(uncompressed_size as u32).to_le_bytes())
-            //     .unwrap();
-
-            // let max_compressed_size = lz4_flex::block::get_maximum_output_size(uncompressed_size);
-            // temp_buffer.resize(uncompressed_size + max_compressed_size, 0);
-
-            // let (input, output) = temp_buffer.split_at_mut(uncompressed_size);
-
-            // let compressed_len = lz4_flex::compress_into(input, output).unwrap();
-            // writer.write_all(&output[..compressed_len]).unwrap();
-
-            // println!(
-            //     "Compressed size: {} / {}",
-            //     compressed_len, uncompressed_size
-            // );
         }
         SerdeFormat::Toml => {
             let s = toml::to_string(&value).unwrap().normalize();
             writer.write_all(s.as_bytes()).unwrap();
         }
-        SerdeFormat::Scn => todo!(),
+        SerdeFormat::Scn => {
+            serde_lua::to_writer(temp_buffer, &value).unwrap();
+
+            let uncompressed_size = temp_buffer.len();
+            writer
+                .write_all(&(uncompressed_size as u32).to_le_bytes())
+                .unwrap();
+
+            let max_compressed_size = lz4_flex::block::get_maximum_output_size(uncompressed_size);
+            temp_buffer.resize(uncompressed_size + max_compressed_size, 0);
+
+            let (input, output) = temp_buffer.split_at_mut(uncompressed_size);
+            let compressed_len = lz4_flex::compress_into(input, output).unwrap();
+            writer.write_all(&output[..compressed_len]).unwrap();
+        }
     }
 }
 
@@ -95,23 +90,6 @@ pub fn deserialize_from<T: DeserializeOwned, R: Read>(
         }
         SerdeFormat::Bincode => {
             reader.read_to_end(temp_buffer)?;
-            // let uncompressed_size =
-            //     u32::from_le_bytes(temp_buffer[0..4].try_into().unwrap()) as usize;
-            // let compressed_start = 4;
-            // let compressed_len = temp_buffer.len() - compressed_start;
-
-            // // Reserve space for decompressed data at the end
-            // temp_buffer.resize(compressed_start + compressed_len + uncompressed_size, 0);
-
-            // let (compressed_part, decompressed_part) =
-            //     temp_buffer.split_at_mut(compressed_start + compressed_len);
-            // let compressed = &compressed_part[compressed_start..];
-
-            // let decompressed_len = lz4_flex::decompress_into(compressed, decompressed_part)?;
-            // assert_eq!(
-            //     decompressed_len, uncompressed_size,
-            //     "decompressed size mismatch"
-            // );
 
             let (decoded, read) = bincode::serde::decode_from_slice(
                 temp_buffer.as_slice(),
@@ -119,13 +97,30 @@ pub fn deserialize_from<T: DeserializeOwned, R: Read>(
             )?;
             assert_eq!(read, temp_buffer.len());
 
-            // assert_eq!(
-            //     read, uncompressed_size,
-            //     "binary payload should be fully consumed"
-            // );
-
             Ok(decoded)
         }
-        SerdeFormat::Scn => todo!(),
+        SerdeFormat::Scn => {
+            reader.read_to_end(temp_buffer)?;
+
+            let uncompressed_size =
+                u32::from_le_bytes(temp_buffer[0..4].try_into().unwrap()) as usize;
+            let compressed_start = 4;
+            let compressed_len = temp_buffer.len() - compressed_start;
+
+            // Reserve space for decompressed data at the end
+            temp_buffer.resize(compressed_start + compressed_len + uncompressed_size, 0);
+
+            let (compressed_part, decompressed_part) =
+                temp_buffer.split_at_mut(compressed_start + compressed_len);
+            let compressed = &compressed_part[compressed_start..];
+
+            let decompressed_len = lz4_flex::decompress_into(compressed, decompressed_part)?;
+            assert_eq!(
+                decompressed_len, uncompressed_size,
+                "decompressed size mismatch"
+            );
+
+            Ok(serde_lua::from_slice(decompressed_part)?)
+        }
     }
 }
