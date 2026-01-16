0cf7f896ff19b43ef1a2a7fd3070e17f0e89aab0
xxorza <xxorza@gmail.com>
2026-01-16 20:46:44 +0200

    Clear argument values cache when computation changes

--- editor/src/app_data.rs
+++ editor/src/app_data.rs
@@ -329,6 +329,7 @@ impl AppData {
 
             if actions.iter().any(|action| action.affects_computation()) {
                 self.execution_stats = None;
+                self.argument_values_cache.clear();
                 graph_updated = true;
             }
         }
