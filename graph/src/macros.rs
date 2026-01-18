b58d620dc754e8cc673c445ed5fe05bb99874242
xxorza <xxorza@gmail.com>
2026-01-18 15:46:59 +0200

    Refactor async_lambda to use state and event_states

--- graph/src/macros.rs
+++ graph/src/macros.rs
@@ -1,24 +1,24 @@
 #[macro_export]
 macro_rules! async_lambda {
-    (move |$ctx_manager:pat_param, $cache:pat_param, $inputs:pat_param, $outputs_meta:pat_param, $outputs:pat_param| { $($name:ident = $init:expr),* $(,)? } => $body:block) => {
-        $crate::lambda::FuncLambda::new(move |$ctx_manager, $cache, $inputs, $outputs_meta, $outputs, _event_caches| {
+    (move |$ctx_manager:pat_param, $state:pat_param, $event_states:pat_param, $inputs:pat_param, $outputs_meta:pat_param, $outputs:pat_param| { $($name:ident = $init:expr),* $(,)? } => $body:block) => {
+        $crate::lambda::FuncLambda::new(move |$ctx_manager, $state, $event_states, $inputs, $outputs_meta, $outputs| {
             $(let $name = $init;)*
             Box::pin(async move $body)
         })
     };
-    (|$ctx_manager:pat_param, $cache:pat_param, $inputs:pat_param, $outputs_meta:pat_param, $outputs:pat_param| { $($name:ident = $init:expr),* $(,)? } => $body:block) => {
-        $crate::lambda::FuncLambda::new(|$ctx_manager, $cache, $inputs, $outputs_meta, $outputs, _event_caches| {
+    (|$ctx_manager:pat_param, $state:pat_param, $event_states:pat_param, $inputs:pat_param, $outputs_meta:pat_param, $outputs:pat_param| { $($name:ident = $init:expr),* $(,)? } => $body:block) => {
+        $crate::lambda::FuncLambda::new(|$ctx_manager, $state, $event_states, $inputs, $outputs_meta, $outputs| {
             $(let $name = $init;)*
             Box::pin(async move $body)
         })
     };
-    (move |$ctx_manager:pat_param, $cache:pat_param, $inputs:pat_param, $outputs_meta:pat_param, $outputs:pat_param| $body:block) => {
-        $crate::lambda::FuncLambda::new(move |$ctx_manager, $cache, $inputs, $outputs_meta, $outputs, _event_caches| {
+    (move |$ctx_manager:pat_param, $state:pat_param, $event_states:pat_param, $inputs:pat_param, $outputs_meta:pat_param, $outputs:pat_param| $body:block) => {
+        $crate::lambda::FuncLambda::new(move |$ctx_manager, $state, $event_states, $inputs, $outputs_meta, $outputs| {
             Box::pin(async move $body)
         })
     };
-    (|$ctx_manager:pat_param, $cache:pat_param, $inputs:pat_param, $outputs_meta:pat_param, $outputs:pat_param| $body:block) => {
-        $crate::lambda::FuncLambda::new(|$ctx_manager, $cache, $inputs, $outputs_meta, $outputs, _event_caches| {
+    (|$ctx_manager:pat_param, $state:pat_param, $inputs:pat_param, $event_states:pat_param, $outputs_meta:pat_param, $outputs:pat_param| $body:block) => {
+        $crate::lambda::FuncLambda::new(|$ctx_manager, $state, $event_states, $inputs, $outputs_meta, $outputs| {
             Box::pin(async move $body)
         })
     };
